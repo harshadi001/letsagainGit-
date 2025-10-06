b4426b8e50da42f42c86b885ce8f5c954793503c
Harsh Adi <harshadisingh45@gmail.com>
2025-10-06 23:23:01 +0530

    push to git oct 6

--- src/ClassOct62025.java
+++ src/ClassOct62025.java
@@ -1,9 +1,35 @@
-
-public class ClassOct62025 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+package softwareTestingMaterial;
+import org.testng.annotations.Test;
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class ClassOct62025{
+	@Test
+	public void testMultipleWindows() throws InterruptedException{
+	System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\src\\test\\java\\drivers\\geckodriver.exe");
+	// To open browser
+	WebDriver driver=new FirefoxDriver();
+	// To maximize browser
+	driver.manage().window().maximize();
+	// To open Naukri website with multiple windows
+	driver.get("http://www.indeed.com/");
+	// It will return the parent window name as a String
+	String mainWindow=driver.getWindowHandle();
+	// It returns no. of windows opened by WebDriver and will return Set of Strings
+	Set<String> set =driver.getWindowHandles();
+	// Using Iterator to iterate with in windows
+	Iterator<String> itr= set.iterator();
+	while(itr.hasNext()){
+		String childWindow=itr.next();
+	   	// Compare whether the main windows is not equal to child window. If not equal, we will close.
+		if(!mainWindow.equals(childWindow)){
+		driver.switchTo().window(childWindow);
+		System.out.println(driver.switchTo().window(childWindow).getTitle());
+		driver.close();
+		}
 	}
-
+	// This is to switch to the main window
+	driver.switchTo().window(mainWindow);
+	}
 }
---
+++ src/HelloWorldGit.java
@@ -0,0 +1,6 @@
+
+public class HelloWorldGit {
+
+	
+	test data
+}
